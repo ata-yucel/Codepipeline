b3323ef32eafad1616f2cca65a932c717708ebdf
Ata Yucel <atayucel184@gmail.com>
2025-11-10 11:32:15 +0300

    first commit

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,8 +3,12 @@ set -e
 
 echo ">>> [CodeDeploy] Running install.sh..."
 
+APP_DIR="/home/ec2-user/simple-node-app"
+cd "$APP_DIR" || exit 1
 
-cd /home/ec2-user/simple-node-app || exit 1
+
+echo ">>> Fixing directory permissions..."
+sudo chown -R ec2-user:ec2-user "$APP_DIR"
 
 
 if ! command -v node &> /dev/null; then
@@ -13,8 +17,7 @@ if ! command -v node &> /dev/null; then
   sudo yum install -y nodejs
 fi
 
-
 echo ">>> Installing npm dependencies..."
-npm install --production
+npm install --omit=dev
 
 echo ">>> [CodeDeploy] install.sh completed successfully."
