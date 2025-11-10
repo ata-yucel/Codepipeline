2b7126d2c0bdff4213532db6f43fcef1a268870e
Ata Yucel <atayucel184@gmail.com>
2025-11-10 12:30:37 +0300

    first commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send(" Hello from CodePipeline Deployment !");
+  res.send(" CLOUD !");
 });
 
 const PORT = process.env.PORT || 3000;
